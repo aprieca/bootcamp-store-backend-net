bc36662cf28a7ffa88fe46c94e87ee8bd5798567
Antonio Prieto <antonioprieto.cr@gmail.com>
2023-05-23 11:39:04 +0200

    02.BACKEND - Configuración de la basde datos en memoria

---
+++ Infrastructure/Persistence/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using bootcamp_store_backend.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace bootcamp_store_backend.Infrastructure.Persistence;
+
+public class StoreContext:DbContext
+{
+    public StoreContext(DbContextOptions<StoreContext> options) : base(options){ }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Item> Items { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using bootcamp_store_backend.Infraestructure.Persistence;
+using bootcamp_store_backend.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +11,25 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<StoreContext>(options => 
+        options.UseInMemoryDatabase(connectionString)
+        );
+}
+
 var app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    DevelopmentDataLoader dataLoader = new(context);
+    dataLoader.LoadData();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
